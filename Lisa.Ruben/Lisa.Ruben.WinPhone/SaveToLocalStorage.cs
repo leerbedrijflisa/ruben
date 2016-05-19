1dd480346c2b3816d158903c3eb492722032c794
VincentVanWijk <99030609@mydavinci.nl>
2016-05-19 11:06:14 +0200

    winphone now saves images correclyto local folder, TODO retrieve them correctly in the pictotheek

--- Lisa.Ruben/Lisa.Ruben.WinPhone/SaveToLocalStorage.cs
+++ Lisa.Ruben/Lisa.Ruben.WinPhone/SaveToLocalStorage.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using Windows.Storage;
 using Xamarin.Forms;
 
@@ -12,15 +13,18 @@ namespace Lisa.Ruben.WinPhone
     {
         public async Task SaveToLocalFolderAsync(Stream file, string fileName)
         {
+            fileName = fileName + ".jpg";
+
             StorageFolder localFolder = ApplicationData.Current.LocalFolder;
             StorageFile storageFile = await localFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
             System.Diagnostics.Debug.WriteLine("----------------------------------------------------------");
             System.Diagnostics.Debug.WriteLine(localFolder);
 
-            using (Stream outputStream = await storageFile.OpenStreamForWriteAsync())
-            {
-                await file.CopyToAsync(outputStream);
-            }
+            Stream outputStream = await storageFile.OpenStreamForWriteAsync();
+
+            file.Seek(0, SeekOrigin.Begin);
+            outputStream.Seek(0, SeekOrigin.Begin);
+            await file.CopyToAsync(outputStream);
         }
 
         public string GetPath()
